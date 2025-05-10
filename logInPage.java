ab67bbe55a9a58512c8cd536dfe4e4818d1f2178
ryanMathram <168239412+ryanMathram@users.noreply.github.com>
2025-05-10 00:25:43 -0400

    Added a centralized logInPage.java and signUp buttons and set up the pages.

---
+++ logInPage.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class logInPage extends JFrame {
+    public logInPage() {
+        JFrame login = new JFrame("Log In");
+        login.setSize(2000, 2000);
+        login.setLayout(null);
+
+        Label ran = new Label("Log In");
+        ran.setBounds(550,150,600,250);
+        ran.setFont(new Font("Times New Roman",Font.PLAIN,80));
+
+        login.add(ran);
+
+        login.setVisible(true);
+    }
+}
+
--- main.java
+++ main.java
@@ -3,24 +3,37 @@ import java.awt.*;
 
 public class main{
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My First GUI");
+        JFrame frame = new JFrame("Volunteering Hub");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
+        frame.setSize(2000,2000);
         frame.setLayout(null);
 
+
         Label label = new Label("Volunteering Hub");
-        label.setBounds(250,100,250,250);
-        JButton button = new JButton("Events");
-        button.setBounds(150,400,100,100);
-        JButton button2 = new JButton("User");
-        button2.setBounds(350,400,100,100);
+        label.setBounds(550,150,600,250);
+        label.setFont(new Font("Times New Roman",Font.PLAIN,80));
+        JButton button = new JButton("Log In");
+        button.setBounds(700,500,300,60);
+        button.setFont(new Font("Times New Roman",Font.PLAIN,15));
+        JButton button2 = new JButton("Sign Up");
+        button2.setBounds(700,600,300,60);
+        button2.setFont(new Font("Times New Roman",Font.PLAIN,15));
+        ImageIcon logo = new ImageIcon("tungtungsahur.png");
+        JLabel logoLabel = new JLabel(logo);
+        logoLabel.setBounds(0,0,logo.getIconWidth(),logo.getIconHeight());
+        frame.add(logoLabel);
 
         frame.add(label);
         frame.getContentPane().add(button); // Adds Button to content pane of frame
         frame.getContentPane().add(button2);
-
+        button.addActionListener(e ->{
+                frame.setVisible(false);
+                new logInPage();
+        });
 
         frame.setVisible(true);
 
     }
 }
+
+
